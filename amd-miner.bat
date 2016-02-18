26bf780cb8ffc42d747a90897dd52f1e05f40af3
Mentor <mentor@palokaj.co>
2016-02-18 17:46:12 +0100

    other pull

--- amd-miner.bat
+++ amd-miner.bat
@@ -8,7 +8,8 @@ cd miners
 echo "This script automates the mining process with etherpool.co, it is not affiliated to etherpool"
 echo "The accounts on this computer are:"
 geth account list
-set /p addr=What address to you want to mine to? (default - '0xd54f0a1c743c432d7af0db15c2d87f483b52439b'):
+set defaddr=0xd54f0a1c743c432d7af0db15c2d87f483b52439b
+set /p addr="What address to you want to mine to?: default is %defaddr%"
 cls
 
 
@@ -19,11 +20,8 @@ cls
 
 ethminer -G -M
 echo "---------------------------------"
-echo "What number of Mega Hash did your device get? Remove the last 6 numbers to calculate it."
-echo "The Mega Hash number should be between 1 and 90."
-echo "NEVER OVER ESTIATE YOUR HASHRATE, you will earn nothing at all if you do"
-
-set /p hashrate=What is your Mega Hash rate? (default - 2):
+echo "The INNER MEAN minus the last 6 numbers should be between 1 and 90"
+set /p hashrate=What is the number after INNER MEAN without the last 6 numbers? (default - 2):
 cls
 
 
