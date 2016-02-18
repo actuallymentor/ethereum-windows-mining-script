05a9fa2a610c1c35eda01897856ffc0376d1caeb
Mentor <mentor@palokaj.co>
2016-02-18 17:41:01 +0100

    created some space

--- nvidia-miner.bat
+++ nvidia-miner.bat
@@ -1,13 +1,22 @@
 @echo off
 echo "Always keep a backup of you wallet files in %appdata%\Ethereum\Keystore"
+pause
+cls
+
+
 cd miners
 echo "This script automates the mining process with etherpool.co, it is not affiliated to etherpool"
 echo "The accounts on this computer are:"
 geth account list
 set /p addr=What address to you want to mine to? (default - '0xd54f0a1c743c432d7af0db15c2d87f483b52439b'):
+cls
+
 
 echo "We will now benchmark your card, let this run"
 pause
+cls
+
+
 ethminer -U -M
 echo "---------------------------------"
 echo "What number of Mega Hash did your device get? Remove the last 6 numbers to calculate it."
@@ -15,5 +24,9 @@ echo "The Mega Hash number should be between 1 and 90."
 echo "NEVER OVER ESTIATE YOUR HASHRATE, you will earn nothing at all if you do"
 
 set /p hashrate=What is your Mega Hash rate? (default - 2):
+cls
 
+
+echo "Mining to %addr%"
+echo "Mining hashrate of %hashrate%"
 ethminercuda -U -F http://pool.eth.pp.ua/?miner=%hashrate%@%addr%@rigname
