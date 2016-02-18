dbb80e703fe7810955bc7edcf8785c0a21873e05
Mentor <mentor@palokaj.co>
2016-02-18 17:33:44 +0100

    first draft done

--- amd-miner.bat
+++ amd-miner.bat
@@ -1,8 +1,12 @@
 @echo off
+cd miners
+echo "This script automates the mining process with etherpool.co, it is not affiliated to etherpool"
+echo "The accounts on this computer are:"
+geth account list
 set /p addr=What address to you want to mine to? (default - 0xd54f0a1c743c432d7af0db15c2d87f483b52439b):
 set /p specAddr=Is your card AMD or Nvidia? [amd/nvidia] (default - amd):
 
-echo "We will now benchmark your card, let this run for a while"
+echo "We will now benchmark your card, let this run"
 pause
 ethminer -G -M
 echo "---------------------------------"
@@ -12,6 +16,3 @@ echo "NEVER OVER ESTIATE YOUR HASHRATE, you will earn nothing at all if you do"
 
 set /p hashrate=What is your Mega Hash rate? (default - 2):
 ethminer -G --cl-global-work 16384 -F http://pool.eth.pp.ua/?miner=%hashrate%@%addr%@script
-
-cd miners
-ethminercuda -U -F http://pool.eth.pp.ua/?miner=5@addr@rigname
--- nvidia-miner.bat
+++ nvidia-miner.bat
@@ -1,9 +1,13 @@
 @echo off
+cd miners
+echo "This script automates the mining process with etherpool.co, it is not affiliated to etherpool"
+echo "The accounts on this computer are:"
+geth account list
 set /p addr=What address to you want to mine to? (default - 0xd54f0a1c743c432d7af0db15c2d87f483b52439b):
 
-echo "We will now benchmark your card, let this run for a while"
+echo "We will now benchmark your card, let this run"
 pause
-ethminer -G -M
+ethminer -U -M
 echo "---------------------------------"
 echo "What number of Mega Hash did your device get? Remove the last 6 numbers to calculate it."
 echo "The Mega Hash number should be between 1 and 90."
@@ -11,5 +15,4 @@ echo "NEVER OVER ESTIATE YOUR HASHRATE, you will earn nothing at all if you do"
 
 set /p hashrate=What is your Mega Hash rate? (default - 2):
 
-cd miners
 ethminercuda -U -F http://pool.eth.pp.ua/?miner=%hashrate%@%addr%@rigname
