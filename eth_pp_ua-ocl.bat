ac049520bc04e96202d16fdc2990b8565df541ce
Mentor <mentor@palokaj.co>
2016-02-18 17:21:13 +0100

    added geth

--- eth_pp_ua-ocl.bat
+++ eth_pp_ua-ocl.bat
@@ -1,1 +1,8 @@
+@echo off
+set /p specAddr=What address to you want to mine to? (default - 0xd54f0a1c743c432d7af0db15c2d87f483b52439b):
+IF %specAddr%=='y'(
+set /p specAddr=Where do you want to mine to? :
+)
+
+
 ethminer -G --cl-global-work 16384 -F http://pool.eth.pp.ua/?miner=5@0x54e8efbd6138011efe9b028e489f6a0bc650e451@rigname
