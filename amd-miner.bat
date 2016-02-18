e0b0cbeb8ff5c2d5ff01e47ab59b42014462ce1d
Mentor <mentor@palokaj.co>
2016-02-18 17:38:18 +0100

    removed card ask

--- amd-miner.bat
+++ amd-miner.bat
@@ -1,10 +1,10 @@
 @echo off
+echo "Always keep a backup of you wallet files in %appdata%\Ethereum\Keystore"
 cd miners
 echo "This script automates the mining process with etherpool.co, it is not affiliated to etherpool"
 echo "The accounts on this computer are:"
 geth account list
 set /p addr=What address to you want to mine to? (default - 0xd54f0a1c743c432d7af0db15c2d87f483b52439b):
-set /p specAddr=Is your card AMD or Nvidia? [amd/nvidia] (default - amd):
 
 echo "We will now benchmark your card, let this run"
 pause
